9f987ca127007e8a64f94218dd0ad5b98e30306e
iamcham <lastchamchi@paran.com>
2012-12-20 12:38:28 +0900

    ggnotbutton

---
+++ Notepadv2/src/com/android/demo/notepad2/NoteEdit.java
@@ -0,0 +1,13 @@
+package com.android.demo.notepad2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoteEdit extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.note_edit);
+	}
+	
+}
